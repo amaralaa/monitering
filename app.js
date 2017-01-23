2c10c7f4466e5b94d50409e341a84d5b7969d0d2
Amar Alaa <x.net87@gmail.com>
2017-01-23 19:21:43 +0300

    Almost Done

---
+++ app.js
@@ -0,0 +1,67 @@
+const exec = require('child_process').exec;
+const readline = require('readline');
+var hosts = [];
+var stop = false;
+var BgRed = "\x1b[41m";
+var BgGreen = "\x1b[42m";
+var BgYellow = "\x1b[43m";
+var Reset = "\x1b[0m";
+var clear = '\033c';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.input.on('keypress', (str,key)=>{
+    // utils.clearScreen(rl);
+    rl.clearLine(null,0);
+    console.log('\x1B[?25h','Good bye ...');
+    process.exit(0);
+});
+
+for(var i = 2; i <= process.argv.length ; i++){
+    if(process.argv[i] !== undefined){
+        hosts.push(process.argv[i]);
+    }
+}
+if(!hosts.length){
+    hosts.push('google.com');
+    hosts.push('8.8.8.8');
+    hosts.push('1.1.1.1');
+    hosts.push('localhost');
+}
+
+function ping (host,stop,cb){
+    (function pinghost(host){exec('ping -c 1 -n '+host,(e,stdOut,stdErr)=>{
+        if(e) {
+            pinghost(host);
+            return cb(e,null);
+        }
+        if(stdErr) {
+            pinghost(host);
+            return cb(stdErr,null)
+        }
+        out = stdOut.match(/time=(.+?) ms/);
+        time = out[0].toString().split('=');
+        output=host + ' is Alive with responce time ' + time[1];
+        pinghost(host);
+        return cb(null,output);
+    })})(host)
+}
+readline.cursorTo(process.stdout,0,0);
+readline.clearScreenDown(process.stdout);
+console.log('Press Any key Any time to exist monitor ....','\x1B[?25l')
+if(hosts.length){
+    hosts.forEach(function(host,i) {
+        new ping(host,stop,(e,time)=>{
+            if(e){
+                readline.cursorTo(process.stdout,0,i+2);
+                readline.clearLine(process.stdout,0);
+                return console.log(host + ' ====> ',BgRed,host + ' Error check this host',Reset);
+            }
+            readline.cursorTo(process.stdout,0,i+2);
+            readline.clearLine(process.stdout,0);
+            return console.log(host + ' ====> ',BgGreen,time,Reset,'\r');
+        })
+    }, this);
+}
